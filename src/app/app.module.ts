42769fbeb2e25f0dadfa65cd575bfb725db62fe0
Maksym Zinchenko <maklaut98@gmail.com>
2022-12-14 21:34:09 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PasswordInputComponent } from './password-input/password-input.component';
+import { ScaleComponent } from './scale/scale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordInputComponent,
+    ScaleComponent
   ],
   imports: [
     BrowserModule
